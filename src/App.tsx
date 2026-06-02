418d4265f2ef0ec567a22490f382a537335078c1
SoupCat <soupcat.py@gmail.com>
2026-06-02 11:04:23 -0600

    guh

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { H1, H2, Para } from "./components/Text";
 
 //          DARK     LIGHT-1      LIGHT-2
 // bg1   taupe-900  olive-200    taupe-400
@@ -15,28 +16,35 @@
 // like instead of amber-950 it would be taupe-800
 
 export default function App() {
+    
+  const LightFgColor = "emerald";
+  const LightBgColor = "olive";
+  
+  const DarkFgColor = "amber";
+  const DarkBgColor = "taupe";
+  
   return (<>
-    <div className={"flex flex-col align-top justify-start p-6 m-4 gap-6 rounded-xl bg-olive-50 dark:bg-taupe-800"}>
+    <div className={`flex flex-col align-top justify-start p-6 m-4 gap-6 rounded-xl bg-${LightBgColor}-50 dark:bg-${DarkBgColor}-800`}>
       <div className={"flex flex-col align-top justify-start gap-2"}>
-        <h1 className={"text-4xl font-bold tracking-wider text-emerald-700 dark:text-amber-500"}>Hi there</h1>
-        <h2 className={"text-2xl font-bold text-emerald-600 dark:text-amber-600"}>guh</h2>
-        <p className={"text-lg text-emerald-900 dark:text-amber-100"}>I'm currently trying to use Tailwind CSS</p>
+        <h1 className={`text-4xl font-bold tracking-wider text-${LightFgColor}-700 dark:text-${DarkFgColor}-500`}>Hi there</h1>
+        <h2 className={`text-2xl font-bold text-${LightFgColor}-600 dark:text-${DarkFgColor}-600`}>guh</h2>
+        <p className={`text-lg text-${LightFgColor}-900 dark:text-${DarkFgColor}-100`}>I'm currently trying to use Tailwind CSS</p>
       </div>
       <a
         href={"https://soupcat.vercel.app"} target={"_blank"}
-        className={"rounded-full w-max bg-emerald-400 dark:bg-amber-500 text-black py-2 px-6 shadow-md transition-transform  hover:scale-105"}
+        className={`rounded-full w-max bg-${LightFgColor}-400 dark:bg-${DarkFgColor}-500 text-black py-2 px-6 shadow-md transition-transform  hover:scale-105`}
       >
         A button!
       </a>
     </div>
 
-    <div className={"flex flex-col align-top justify-start p-6 m-4 gap-6 rounded-xl bg-olive-50 dark:bg-taupe-800"}>
+    <div className={`flex flex-col align-top justify-start p-6 m-4 gap-6 rounded-xl bg-${LightBgColor}-50 dark:bg-${DarkBgColor}-800`}>
       <div className={"flex flex-col align-top justify-start gap-2"}>
-        <h1 className={"text-4xl font-bold tracking-wider text-emerald-700 dark:text-amber-500"}>Here's another card</h1>
-        <h2 className={"text-2xl font-bold text-emerald-600 dark:text-amber-600"}>idk if this works</h2>
-        <p className={"text-lg text-emerald-900 dark:text-amber-100"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
-        <h2 className={"text-2xl font-bold text-emerald-600 dark:text-amber-600"}>oh look another subheader</h2>
-        <p className={"text-lg text-emerald-900 dark:text-amber-100"}>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <H1>Look another card</H1>
+        <H2>About</H2>
+        <Para>I'm testing using little pre-styled components for text. It seems to be working really well :)</Para>
+        <H2>Lots of stuff</H2>
+        <Para>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien mi, pulvinar vel consequat quis, varius eget leo. Aenean ut sapien nisi. Ut finibus dolor ac quam euismod, a dignissim elit placerat. Ut nec orci id odio euismod volutpat quis aliquet ipsum. Aliquam dapibus condimentum volutpat. Mauris eu felis justo.</Para>
       </div>
     </div>
   </>)
