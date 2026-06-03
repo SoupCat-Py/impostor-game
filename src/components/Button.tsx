4d96737cafcb8ee87946ff416ef90e6f46cc0efc
Soup Cat <soupcat.py@gmail.com>
2026-06-02 20:38:15 -0600

    reformatting stuff
    set indents to 2 spaces

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,33 @@
-import type { ReactNode } from "react";
+import type {ReactNode} from "react";
 
 type buttonProps = {
-    children: ReactNode;
-    primary?: boolean;
-    link?: boolean;
+  children: ReactNode;
+  primary?: boolean;
+  link?: boolean;
 }
 
-export default function Button({children, primary=false, link=false}:buttonProps) {
-    if (link) {
-        return (
-            <a className={`
-                flex justify-center items-center py-2 w-full
-                rounded-full font-bold border-3 
-                cursor-pointer select-none
-                ${primary
-                    ?"bg-emerald-400 border-emerald-400 dark:bg-amber-500 dark:border-amber-500 text-white dark:text-black"
-                    :"bg-none border-emerald-700 dark:border-amber-100 text-emerald-700 dark:text-amber-100"
-                }
-                ${primary
-                    ?"shadow-emerald-700 dark:shadow-amber-800 active:translate-y-[3px]"
-                    :"shadow-none"}
-                shadow-[0_3px_0_0] active:shadow-none transition duration-100
-                ${primary
-                    ?"hover:bg-emerald-500 hover:border-emerald-500 hover:dark:bg-amber-600 hover:dark:border-amber-600"
-                    :"hover:bg-emerald-200 hover:dark:bg-taupe-700/50"}
-                `}
-            >
-                {children}
-            </a>
-        )
-    }
+export default function Button({children, primary = false, link = false}: buttonProps) {
+  if (link) {
+    return (
+      <a className={`
+        flex justify-center items-center py-2 w-full
+        rounded-full font-bold border-3 
+        cursor-pointer select-none
+        ${primary
+          ? "bg-emerald-400 border-emerald-400 dark:bg-amber-500 dark:border-amber-500 text-white dark:text-black"
+          : "bg-none border-emerald-700 dark:border-amber-100 text-emerald-700 dark:text-amber-100"
+        }
+        ${primary
+          ? "shadow-emerald-700 dark:shadow-amber-800 active:translate-y-[3px]"
+          : "shadow-none"}
+        shadow-[0_3px_0_0] active:shadow-none transition duration-100
+        ${primary
+          ? "hover:bg-emerald-500 hover:border-emerald-500 hover:dark:bg-amber-600 hover:dark:border-amber-600"
+          : "hover:bg-emerald-200 hover:dark:bg-taupe-700/50"}
+        `}
+      >
+        {children}
+      </a>
+    )
+  }
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,16 +1,16 @@
 import type { ReactNode } from "react"
 
 type TextProps = {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 
 export function H1({children}:TextProps) {
-    return (
-        <h1 className="text-4xl pb-2 font-bold tracking-wide text-emerald-700 dark:text-amber-500">
-            {children}
-        </h1>
-    )
+  return (
+    <h1 className="text-4xl pb-2 font-bold tracking-wide text-emerald-700 dark:text-amber-500">
+      {children}
+    </h1>
+  )
 }
 
 // NOTE FOR FUTURE IF LAYOUTS LOOK ODD
@@ -20,17 +20,17 @@ export function H1({children}:TextProps) {
 // may introduce some problems later so be aware of  that
 
 export function H2({children}:TextProps) {
-    return (
-        <h2 className="text-2xl pt-2 font-bold text-emerald-600 dark:text-amber-600">
-            {children}
-        </h2>
-    )
+  return (
+    <h2 className="text-2xl pt-2 font-bold text-emerald-600 dark:text-amber-600">
+      {children}
+    </h2>
+  )
 }
 
 export function Para({children}:TextProps) {
-    return (
-        <p className="text-md text-emerald-900 dark:text-amber-100">
-            {children}
-        </p>
-    )
+  return (
+    <p className="text-md text-emerald-900 dark:text-amber-100">
+      {children}
+    </p>
+  )
 }
