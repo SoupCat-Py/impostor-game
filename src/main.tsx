bf0da44d9aa90d0efa7e11faff4e5ac43b92adc7
SoupCat <soupcat.py@gmail.com>
2026-06-11 08:50:38 -0600

    moved component playground to its own file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import ComponentPlayground from './pages/Testing.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ComponentPlayground />
   </StrictMode>,
 )
