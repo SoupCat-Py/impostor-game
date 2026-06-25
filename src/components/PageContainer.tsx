8829e2e8206762ec61048a63d69c817553db3db6
SoupCat <soupcat.py@gmail.com>
2026-06-24 19:19:31 -0600

    DVH WHY DIDN'T I DO THIS BEFORE

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -6,7 +6,7 @@ type PageContainerProps = {
 
 export default function PageContainer({children}:PageContainerProps) {
   return (
-    <main className="flex flex-col p-6 gap-6 min-h-screen max-w-150 place-content-between">
+    <main className="flex flex-col p-6 gap-6 min-h-[100dvh] max-w-150 place-content-between">
         {children}
     </main>
   )
